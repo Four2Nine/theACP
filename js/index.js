378ca815fa32dd2e48228c1fd4c6ca336b912fc0
sealiu <sealiu0217@gmail.com>
2016-11-07 18:15:21 +0800

    修改首页轮播从数据库中读取数据

--- js/index.js
+++ js/index.js
@@ -26,6 +26,28 @@ $(document).ready(function () {
         }
     });
 
+    $.ajax({
+        url: "/theACP/controller/index.con.php",
+        success: function (data) {
+            var result = JSON.parse(data);
+            var slideBubble = "";
+            var mainSlider = $("#main-slider");
+
+            for (var item in result) {
+                if (item == 0) {
+                    slideBubble += '<li data-target="#main-slider" data-slide-to="' + item + '" class="active"></li>';
+                } else {
+                    slideBubble += '<li data-target="#main-slider" data-slide-to="' + item + '"></li>';
+                }
+                var slideItem = $("#cu-item" + item);
+                slideItem.find("h2").html(result[item]["title"]);
+                slideItem.find("p").html(result[item]["subtitle"]);
+            }
+
+            mainSlider.find("ol").html(slideBubble);
+        }
+    });
+
     //退出登录
     $("#cu-logout").click(function () {
         $.ajax({
